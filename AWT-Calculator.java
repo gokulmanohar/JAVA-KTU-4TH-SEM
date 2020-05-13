53aecc620b093db1085c61fb577d68811d7f1c7d
Gokul Manohar <gokulmanohar7@gmail.com>
2020-05-13 19:02:25 +0530

    Update AWT-Calculator.java

--- AWT-Calculator.java
+++ AWT-Calculator.java
@@ -1,227 +1,219 @@
-import java.awt.*;  
-import java.awt.event.*; 
-class AWT_Calculator extends Frame implements ActionListener
+import java.awt.*;
+import java.awt.event.*;
+class calculator implements ActionListener
 {
-Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9, plus,minus,multiply,divide,equal,clear,dec;
-TextField tf; 
-float res;
-static String stext="";
-AWT_Calculator()
-{
-b0=new Button("0");
-b1=new Button("1");
-b2=new Button("2");
-b3=new Button("3");
-b4=new Button("4");
-b5=new Button("5");
-b6=new Button("6");
-b7=new Button("7");
-b8=new Button("8");
-b9=new Button("9");
-plus=new Button("+");
-minus=new Button("-");
-multiply=new Button("*");
-divide=new Button("/");
-equal=new Button("=");
-clear=new Button("c");
-dec=new Button(".");
-tf = new TextField();
-  
-//setBounds(int x-coordinate, int y-coordinate, int width, int height)
-tf.setBounds(30,60,230,30);
-b7.setBounds(30,160,50,30);
-b8.setBounds(70,160,50,30);
-b9.setBounds(110,160,50,30);
-b1.setBounds(30,100,50,30);
-b2.setBounds(70,100,50,30);
-b3.setBounds(110,100,50,30);
-b4.setBounds(30,130,50,30);
-b5.setBounds(60,130,60,30);
-b6.setBounds(110,130,50,30);
-
-
-
-b0.setBounds(60,190,50,30);
-plus.setBounds(150,100,60,30);
-minus.setBounds(150,130,60,30);
-multiply.setBounds(200,100,60,30);
-divide.setBounds(200,130,60,30);
-equal.setBounds(150,160,60,30);
-equal.setBackground(Color.YELLOW);
-clear.setBounds(200,160,60,30);
-dec.setBounds(30,190,50,30);
-tf.setEditable(false);
-	
-b0.addActionListener(this);
-b1.addActionListener(this);
-b2.addActionListener(this);
-b3.addActionListener(this);
-b4.addActionListener(this);
-b5.addActionListener(this);
-b6.addActionListener(this);
-b7.addActionListener(this);
-b8.addActionListener(this);
-b9.addActionListener(this);
-plus.addActionListener(this);
-minus.addActionListener(this);
-multiply.addActionListener(this);
-divide.addActionListener(this);
-equal.addActionListener(this);
-clear.addActionListener(this);
-dec.addActionListener(this);
-addWindowListener(new WindowAdapter(){
-	public void windowClosing(WindowEvent e)
-{
-	dispose();
-}
-});	
-
-add(tf);
-add(b0);
-add(b1);
-add(b2);
-add(b3);
-add(b4);
-add(b5);
-add(b6);
-add(b7);
-add(b8);
-add(b9);
-add(plus);
-add(minus);
-add(multiply);
-add(divide);
-add(equal);
-add(clear);
-add(dec);
-setSize(300,300);
-setLayout(null);
-setVisible(true);	
-}
-public void actionPerformed(ActionEvent e)
-{
-// overriding the ActionListener interface method	
-	if(e.getSource() == b0)
-	{
-		stext=stext+"0";
-		tf.setText(stext);
-	}	
-	
-	if(e.getSource() == b1)
-	{
-		stext=stext+"1";
-		tf.setText(stext);
-	}	
-	
-	if(e.getSource() == b2)
-	{
-		stext=stext+"2";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == b3)
-	{
-		stext=stext+"3";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == b4)
-	{
-		stext=stext+"4";
-		tf.setText(stext);
-	}
-	if(e.getSource() == b5)
-	{
-		stext=stext+"5";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == b6)
-	{
-		stext=stext+"6";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == b7)
-	{
-		stext=stext+"7";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == b8)
-	{
-		stext=stext+"8";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == b9)
-	{
-		stext=stext+"9";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == plus)
-	{
-		stext=stext+" + ";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == minus)
-	{
-		stext=stext+" - ";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == multiply)
-	{
-		stext=stext+" * ";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == divide)
-	{
-		stext=stext+" / ";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == dec)
-	{
-		stext=stext+".";
-		tf.setText(stext);
-	}	
-	if(e.getSource() == equal)
-	{
-        String[] words = stext.split(" ");
-		//First operand
-		String w1 = words[0];
-		//Operator
-		String w2 = words[1];
-		//Second Operand
-		String w3 = words[2];
-		//Convertion
-		float op1 = Float.parseFloat(w1);
-		float op2 = Float.parseFloat(w3);
-		if (w2.equals("+"))
-		{
-			res = op1 + op2;
-		}
-		else if (w2.equals("-"))
-		{
-			res = op1 - op2;
-		}
-		else if (w2.equals("*"))
-		{
-			res = op1 * op2;
-		}
-		else if (w2.equals("/"))
-		{
-			res = op1 / op2;
-		}
-		else{
-			tf.setText("Error!");
-		}
-		//Again Convertion
-		String ress = Float.toString(res);
-        tf.setText(ress);
-
-	}	
-	if(e.getSource() == clear)
-	{
-		stext=" ";
-		tf.setText(stext);
-	}
-}
-public static void main(String args[])
-{
-AWT_Calculator calculate = new AWT_Calculator();
-}
+    int c,n;
+    String s1,s2,s3,s4,s5;
+    Frame f;
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17;
+    Panel p;
+    TextField tf;
+    GridLayout g;
+    calculator()
+    {
+        f = new Frame("My calculator");
+        p = new Panel();
+        f.setLayout(new FlowLayout());
+        b1 = new Button("0");
+        b1.addActionListener(this);
+        b2 = new Button("1");
+        b2.addActionListener(this);
+        b3 = new Button("2");
+        b3.addActionListener(this);
+        b4 = new Button("3");
+        b4.addActionListener(this);
+        b5 = new Button("4");
+        b5.addActionListener(this);
+        b6 = new Button("5");
+        b6.addActionListener(this);
+        b7 = new Button("6");
+        b7.addActionListener(this);
+        b8 = new Button("7");
+        b8.addActionListener(this);
+        b9 = new Button("8");
+        b9.addActionListener(this);
+        b10 = new Button("9");
+        b10.addActionListener(this);
+        b11 = new Button("+");
+        b11.addActionListener(this);
+        b12 = new Button("-");
+        b12.addActionListener(this);
+        b13 = new Button("*");
+        b13.addActionListener(this);
+        b14 = new Button("/");
+        b14.addActionListener(this);
+        b15 = new Button("%");
+        b15.addActionListener(this);
+        b16 = new Button("=");
+        b16.addActionListener(this);
+        b17 = new Button("C");
+        b17.addActionListener(this);
+        tf = new TextField(20);
+        f.add(tf);
+        g = new GridLayout(4,4,10,20);
+        p.setLayout(g);
+        p.add(b1);p.add(b2);p.add(b3);p.add(b4);p.add(b5);p.add(b6);p.add(b7);p.add(b8);p.add(b9);
+        p.add(b10);p.add(b11);p.add(b12);p.add(b13);p.add(b14);p.add(b15);p.add(b16);p.add(b17);
+        f.add(p);
+        f.setSize(300,300);
+        f.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==b1)
+        {
+            s3 = tf.getText();
+            s4 = "0";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b2)
+        {
+            s3 = tf.getText();
+            s4 = "1";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b3)
+        {
+            s3 = tf.getText();
+            s4 = "2";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }if(e.getSource()==b4)
+        {
+            s3 = tf.getText();
+            s4 = "3";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b5)
+        {
+            s3 = tf.getText();
+            s4 = "4";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b6)
+        {
+            s3 = tf.getText();
+            s4 = "5";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b7)
+        {
+            s3 = tf.getText();
+            s4 = "6";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b8)
+        {
+            s3 = tf.getText();
+            s4 = "7";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b9)
+        {
+            s3 = tf.getText();
+            s4 = "8";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b10)
+        {
+            s3 = tf.getText();
+            s4 = "9";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b11)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=1;
+ 
+        }
+        if(e.getSource()==b12)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=2;
+ 
+        }
+        if(e.getSource()==b13)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=3;
+ 
+        }
+        if(e.getSource()==b14)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=4;
+ 
+        }
+        if(e.getSource()==b15)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=5;
+ 
+        }
+        if(e.getSource()==b16)
+        {
+            s2 = tf.getText();
+            if(c==1)
+            {
+                n = Integer.parseInt(s1)+Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+            else
+            if(c==2)
+            {
+                n = Integer.parseInt(s1)-Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+            else
+            if(c==3)
+            {
+                n = Integer.parseInt(s1)*Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+            if(c==4)
+            {
+                try
+                {
+                    int p=Integer.parseInt(s2);
+                    if(p!=0)
+                    {
+                    n = Integer.parseInt(s1)/Integer.parseInt(s2);
+                    tf.setText(String.valueOf(n));
+                     }
+                     else
+                        tf.setText("infinite");
+ 
+                }
+                catch(Exception i){}
+            }
+            if(c==5)
+            {
+                n = Integer.parseInt(s1)%Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+        }
+        if(e.getSource()==b17)
+        {
+            tf.setText("");
+        }
+    }
+ 
+    public static void main(String abc[])
+    {
+        calculator v = new calculator();
+    }
 }
