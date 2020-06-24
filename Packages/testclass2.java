82713df1c2007b475e12faa44b95460d52bbd192
Gokul Manohar <gokulmanohar7@gmail.com>
2020-06-24 11:41:03 +0530

    Add files via upload

---
+++ Packages/testclass2.java
@@ -0,0 +1,20 @@
+package packagenew;
+ public class testclass2 
+{
+ int a=5;
+testclass3 obj=new testclass3();
+ public void methodname1() 
+   {
+obj.methodname2();
+ System.out.println("hello11:"+a);
+   }
+}
+
+ class testclass3
+{
+ int a=5;
+ public void methodname2() 
+   {
+    System.out.println("hello2:"+a);
+   }
+}
